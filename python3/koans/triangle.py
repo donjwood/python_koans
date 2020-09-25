0766692afb03101d4c5916e6daab55bff8999272
Don Wood <donwood@gmail.com>
2020-09-24 23:34:35 -0500

    Completed 42% of koans.

--- python3/koans/triangle.py
+++ python3/koans/triangle.py
@@ -18,7 +18,39 @@
 #
 def triangle(a, b, c):
     # DELETE 'PASS' AND WRITE THIS CODE
-    pass
+
+    # Check that the triangle is valid
+    if not is_valid_triangle(a, b, c):
+        raise TriangleError("The values {0}, {1}, and {2} do not make a valid triangle".format(a, b, c))
+
+    # Since sets essentially trim down to unique values, by putting 
+    # the values into a set and checking the length, we can see what 
+    # type of triangle it is.
+    triangle_set = set([a, b, c])
+    if len(triangle_set) == 1:
+        return "equilateral"
+    elif len(triangle_set) == 2:
+        return "isosceles"
+    else:
+        return "scalene"
+
+def is_valid_triangle(a, b, c):
+    
+    side_list = [a, b, c]
+
+    # Iterate through the list and insure all the values are greater than 0.
+    for side in side_list:
+        if side <= 0:
+            return False
+
+    # Check that any two sides are always greater than the third.
+    side_total = a + b + c
+    for side in side_list:
+        if side > side_total - side:
+            return False 
+
+    # If we get here, the triangle is valid
+    return True
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError(Exception):
